62d8c01ddd47d04d49bb71a0b517d3612e7f2a1c
CProta <p_car@hotmail.it>
2023-04-06 14:56:54 +0200

    push in prod 2

--- src/test/java/com/coralogix/calculator/services/ModuloServiceTest.java
+++ src/test/java/com/coralogix/calculator/services/ModuloServiceTest.java
@@ -15,6 +15,6 @@ public class ModuloServiceTest {
         Result result = service.Mod(4, 2);
 
         // then
-        assertEquals(result.getResult(), 1);
+        assertEquals(result.getResult(), 0);
     }
 }
